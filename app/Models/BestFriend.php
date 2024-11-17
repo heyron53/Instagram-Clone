91fe8dff49fbb84609b2b9969b569f430b8e33a4
Aarón Morcillo Martínez <heyron53@gmail.com>
2024-11-17 10:53:13 +0100

    ITC-17: Added BestFriend model

--- app/Models/BestFriend.php
+++ app/Models/BestFriend.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class BestFriend extends Model
 {
     use HasFactory;
+
+    protected $table = 'best_friends';
+
+    protected $fillable = [
+        'user_id',
+        'best_friend_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function bestFriend()
+    {
+        return $this->belongsTo(User::class, 'best_friend_id');
+    }
+
 }
