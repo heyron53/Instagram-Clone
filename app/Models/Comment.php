8943ac7c9ea2cbc00150c51a89f25ab4d66455af
Aarón Morcillo Martínez <heyron53@gmail.com>
2024-11-17 11:04:42 +0100

    ITC-18: Added Comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $table = 'comments';
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(User::class, 'post_id');
+    }
+
 }
